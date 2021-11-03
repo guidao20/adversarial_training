bb327829fa4bcac75f7cedf498fbeebee5a4e503
SYD <Sunyd@bupt.edu.cn>
2021-11-03 01:29:44 +0000

    add config.py

--- train_mnist.py
+++ train_mnist.py
@@ -12,6 +12,8 @@ from torch.utils.data import DataLoader, Dataset
 import os
 from mnist_net import mnist_net
 from adversarial_training import Adversarial_Trainings
+from config import get_args
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -19,21 +21,6 @@ logging.basicConfig(
     datefmt='%Y/%m/%d %H:%M:%S',
     level=logging.DEBUG)
 
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batch-size', default=100, type=int)
-    parser.add_argument('--data-dir', default='../mnist-data', type=str)
-    parser.add_argument('--epochs', default=10, type=int)
-    parser.add_argument('--attack', default='fgsm', type=str, choices=['none', 'pgd', 'fgsm'])
-    parser.add_argument('--epsilon', default=0.3, type=float)
-    parser.add_argument('--alpha', default=0.375, type=float)
-    parser.add_argument('--attack-iters', default=40, type=int)
-    parser.add_argument('--lr-max', default=5e-3, type=float)
-    parser.add_argument('--lr-type', default='cyclic')
-    parser.add_argument('--fname', default='mnist_model1', type=str)
-    parser.add_argument('--seed', default=0, type=int)
-    return parser.parse_args()
 
 
 def main():
